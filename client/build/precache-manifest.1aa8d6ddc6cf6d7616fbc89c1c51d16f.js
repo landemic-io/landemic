8df3675a751a19ec6d21435dac980fd4abea012c
Landemic API <admin@landemic.io>
2024-08-07 03:01:22 +0000

    Updated data

---
+++ client/build/precache-manifest.1aa8d6ddc6cf6d7616fbc89c1c51d16f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f7874955c01ba7d0ccccfa2f83f1f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba85eb457f20072b962",
+    "url": "/static/css/main.927b782d.chunk.css"
+  },
+  {
+    "revision": "02974ee92a6ab1eb06dc",
+    "url": "/static/js/2.95ad4754.chunk.js"
+  },
+  {
+    "revision": "0ba85eb457f20072b962",
+    "url": "/static/js/main.6f33ecb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1596bfd4a464a704533791d9884a074c",
+    "url": "/static/media/landemic-logo.1596bfd4.svg"
+  },
+  {
+    "revision": "2980f875bb0f751ffe7d5c4389358746",
+    "url": "/static/media/metamask.2980f875.png"
+  }
+]);
